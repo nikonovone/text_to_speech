afe6d1e7f0df6fa7866b28f846eccf9dc5dfeb56
nikonovone <nikonov.nikita@hotmail.com>
2023-05-05 01:12:10 +0700

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project based on Text-to-Speech technology that allows you to convert text to speech with high accuracy and naturalness. Using advanced speech generation algorithms and a trained model, this project provides a simple and efficient way to create audio content from any text. Thanks to its open structure and simple interface, this project can be used for a variety of applications, from automated audiobooks to creating personal assistants based on voice technology.',
+    author='fit_predict',
+    license='MIT',
+)
